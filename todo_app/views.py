55e0958b7a980ff32b969e908be182c35d2554a4
Mohammad <mohammadfcc2745@gmail.com>
2020-09-25 17:49:08 +0600

    Initial Commit

---
+++ todo_app/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponse, HttpRequest
+from .models import Todo
+
+
+def index(request):
+    context = {
+        'todo_list': Todo.objects.all()
+    }
+    return render(request, 'todo_app/todo_list.html', context)
+
+
+def insert_todo_item(request: HttpRequest):
+    todo = Todo(content=request.POST['content'])
+    todo.save()
+    return redirect('/')
+
+
+def delete_todo_item(request, todo_id):
+    todo_to_delete = Todo.objects.get(id=todo_id)
+    todo_to_delete.delete()
+    return redirect('/')
+
